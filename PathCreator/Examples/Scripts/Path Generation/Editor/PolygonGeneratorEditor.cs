23a0eb42ce054654ed867cae324cdf158e43de61
hadiDanial <hadidanial97@gmail.com>
2019-03-04 23:26:03 +0200

    Removed Time and Limits settings from Polygon Generator editor
    Removed the Time and Limits settings tabs in the Polygon Generator editor. These settings are meaningless for this generator.

--- PathCreator/Examples/Scripts/Path Generation/Editor/PolygonGeneratorEditor.cs
+++ PathCreator/Examples/Scripts/Path Generation/Editor/PolygonGeneratorEditor.cs
@@ -50,10 +50,6 @@ public class PolygonGeneratorEditor : PathGeneratorEditor
             DrawUILine();
             ShowPolygonGenerationSettings(headerStyle);
             DrawUILine();
-            ShowLimits(headerStyle);
-            DrawUILine();
-            ShowTime(headerStyle);
-            DrawUILine();
             ShowCollider(headerStyle);
             }
         GUILayout.EndVertical();
